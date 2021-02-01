ba23508a7caa28c469f3978228c1880232c2346a
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-01 15:29:49 -0300

    Initial commit!

---
+++ src/main/java/com/github/onyxiansoul/MessageUtil.java
@@ -0,0 +1,88 @@
+package com.github.onyxiansoul;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.PluginManager;
+
+public class MessageUtil {
+    private final ConsoleCommandSender CONSOLE_SENDER;
+    private final String WARNING_PREFIX;
+    private final String ERROR_PREFIX;
+    private final String ERROR_SUFIX;
+    private boolean debugEnabled;
+    
+    protected MessageUtil(OnyxianPlugin onyxianPlugin){
+      CONSOLE_SENDER = Bukkit.getConsoleSender();
+      String pluginName = onyxianPlugin.getName();
+      WARNING_PREFIX = System.lineSeparator()+ ChatColor.GOLD+"[WARNING]"+pluginName+" ";
+      ERROR_PREFIX = System.lineSeparator()+ ChatColor.RED+"[ERROR]"+pluginName+" ";
+      ERROR_SUFIX = System.lineSeparator() + ChatColor.RED+ "Disabling"+" "+pluginName + "!";
+      debugEnabled = false;
+    }
+    
+    public void sendWarning(String warningMessage){
+        CONSOLE_SENDER.sendMessage(WARNING_PREFIX + warningMessage);
+    }
+    public void sendWarning(Exception e){
+        String warningMessage = getFormattedException(e);
+        sendWarning(warningMessage);
+    }
+    
+    public void errorDisable(String errorMessage){
+        CONSOLE_SENDER.sendMessage(ERROR_PREFIX + errorMessage + ERROR_SUFIX);
+        PluginManager pluginManager = Bukkit.getServer().getPluginManager();
+        pluginManager.disablePlugin(pluginManager.getPlugin("DamagePotionEffects"));
+    }
+    
+    public void errorDisable(Exception e){
+        String errorMessage = getFormattedException(e);
+        errorDisable(errorMessage);
+    }
+    
+    private String getFormattedException(Throwable ex){
+        StringBuilder sb = new StringBuilder();
+        if(ex.getMessage()!=null){
+          sb.append(ex.getMessage());
+        }
+        else{
+          sb.append(ex.getClass().getSimpleName());
+        }
+        sb.append(" ");
+        if(debugEnabled){
+            sb.append(System.lineSeparator());
+            sb.append(ChatColor.GOLD);
+            sb.append("-------------------------------------");
+            sb.append(System.lineSeparator());
+            sb.append(ChatColor.DARK_GRAY);
+            sb.append(ex.getClass().getName());
+            sb.append(System.lineSeparator());
+            for(StackTraceElement element: ex.getStackTrace()){
+                sb.append(" at: ");
+                sb.append(element);
+                sb.append(System.lineSeparator());
+            }
+            sb.append(ChatColor.GOLD);
+            sb.append("-------------------------------------" );
+        }
+        if(ex.getCause()!=null){
+            sb.append(System.lineSeparator());
+            sb.append("Caused By: " );
+            sb.append(ChatColor.GOLD);
+            sb.append(System.lineSeparator()); //
+            sb.append(getFormattedException(ExceptionUtils.getCause(ex)));
+            //sb.append(getFormattedException(ExceptionUtils.getRootCause(ex)));
+        }
+        else{
+          sb.append(System.lineSeparator());
+          sb.append("---------------------------------------------------------");
+        }
+        return sb.toString();
+    }
+    
+    public void setDebug(boolean debug){
+        debugEnabled = debug;
+    };
+        
+}
---
+++ src/main/java/com/github/onyxiansoul/OnyxianPlugin.java
@@ -0,0 +1,58 @@
+package com.github.onyxiansoul;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+//NOTE! all onyxianplugins must depend on the onyxiancore!
+public abstract class OnyxianPlugin extends JavaPlugin {
+  private final MessageUtil messageUtil;
+  private final String coreVersion;
+  
+  public OnyxianPlugin(String coreVersion){
+    System.out.println("Plugin instanced");
+    this.coreVersion = coreVersion;
+    this.messageUtil = new MessageUtil(this);
+    downloadCoreIfMissing();
+    //fire the evnet here?
+  }
+  
+  @Override
+  public void onLoad(){
+    System.out.println("Plugin loaded");
+  }
+  
+  private void downloadCoreIfMissing(){
+    if(!isCorePresent()){
+      String requiredCoreURL="https://jitpack.io/com/github/OnyxianSoul/OnyxianCore/"+coreVersion+"/OnyxianCore-"+coreVersion+".jar";
+      String pluginsFolder = getDataFolder().getParent()+"/OnyxianCore.jar";
+      try{
+        downloadFile(requiredCoreURL,pluginsFolder);
+      }
+      catch (IOException e){
+          messageUtil.errorDisable("Tried to automatically download the OnyxianCore, but failed"+ System.lineSeparator()
+          +"Since the core is essential for"+ getName() + " Plugin to work, it will disable itself."+ System.lineSeparator()
+          +"Please download the core manually from: " + requiredCoreURL);
+      }
+    }
+  }
+  
+  private boolean isCorePresent(){
+    return Bukkit.getPluginManager().isPluginEnabled("OnyxianCore");
+  }
+  
+  private void downloadFile(String url, String fileName) throws MalformedURLException, IOException{
+      URL website = new URL("https://jitpack.io/com/github/OnyxianSoul/OnyxianCore/5d796199cb/OnyxianCoreAPI-5d796199cb.jar");
+      ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+      FileOutputStream fos = new FileOutputStream("information.html");
+      fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+  }
+
+}
+
+
